6b4434835d44f2b36d65e797c06bc141e6733cf6
Brent <brent.spector@yahoo.com>
2024-04-16 11:06:54 -0700

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+rootProject.name = "universal-pokemon-randomizer-reloaded"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+include(":composeApp")
