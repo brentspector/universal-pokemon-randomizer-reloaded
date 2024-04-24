e38b1a756efa399f943d9ad514738d82de845bbc
Brent <9450120+brentspector@users.noreply.github.com>
2024-04-24 16:40:04 -0700

    Add remaining RomHandlers

---
+++ composeApp/src/commonMain/kotlin/romHandlers/Gen1RomHandler.kt
@@ -0,0 +1,8 @@
+package romHandlers
+
+// Gen4RomHandler.kt
+class Gen1RomHandler : AbstractGBCRomHandler() {
+    override fun parseRom() {
+        println("Gen4 Parsing")
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/romHandlers/Gen2RomHandler.kt
@@ -0,0 +1,8 @@
+package romHandlers
+
+// Gen4RomHandler.kt
+class Gen2RomHandler : AbstractGBCRomHandler() {
+    override fun parseRom() {
+        println("Gen4 Parsing")
+    }
+}
